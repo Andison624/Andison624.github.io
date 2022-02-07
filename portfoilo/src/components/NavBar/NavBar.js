dd74847fc323afc6deb858c68557ffd309023a5f
Andison <andison624@gmail.com>
2022-02-07 20:55:14 +0800

    commit

--- portfoilo/src/components/NavBar/NavBar.js
+++ portfoilo/src/components/NavBar/NavBar.js
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom";
-import  './NavBar.css';
+import "./NavBar.css";
 
 export default function NavBar() {
   return (
-    <div className="nav-menu">
-        <div>
-        <Link to="#" className="title homepage">Andison</Link>
-        </div>
-        <div>
-        <Link to="#" className="title">MyPlan</Link>
-        <Link to="#" className="title">MyProject</Link>
-        <Link to="#" className="title">About</Link>
-        </div>
-    </div>
+    <nav className="nav-menu">
+      <div>
+        <h4>
+          <Link to="#" className="title homepage">
+            Andison
+          </Link>
+        </h4>
+      </div>
+      <div>
+        <h4>
+          <Link to="#" className="title">
+            MyPlan
+          </Link>
+          <Link to="#" className="title">
+            MyProject
+          </Link>
+          <Link to="#" className="title">
+            About
+          </Link>
+        </h4>
+      </div>
+    </nav>
   );
 }
