169d08ad03f8e3992256f693fe5056255b80968f
Andison <andison624@gmail.com>
2022-02-14 23:05:54 +0800

    commit

--- portfoilo/src/components/ProjectList/ProjectList.js
+++ portfoilo/src/components/ProjectList/ProjectList.js
@@ -10,24 +10,61 @@ import $ from "jquery";
 import "./ProjectList.css";
 
 export default function Project() {
-  
-  const [projectList, setProjectList] = useState({
-    pjTitle: "",
-    pjDetail: "",
-  });
+  const pjData = [
+    {
+      id: "colmarAcademy",
+      pjTitle: "Colmar Academy",
+      pjDetail:
+        "My first side project, a web template need to use HTML5, CSS, and Responsive Web Design as project requirements. Outside of the project requirements, I joined the bootstrap framework to complete this side project.",
+      src: ColmarAcademy,
+      alt: "Colmar Academy Side Project",
+      style:{opacity: 1}
+    },
+    {
+      id: "findYourHat",
+      pjTitle: "Find Your Hat",
+      pjDetail:
+      "This is my first side project after learning JavaScript, only using four patterns and JS language to create this mini-game that can run in a terminal. To add challenges, in addition to the project requirements, I added Hardmode to make this mini-game more  interesting.",
+      src: FindYourHat,
+      alt: "Find Your Hat Side Project",
+      style:{opacity: 2}
+    },
+    {
+      id: "numberGuesser",
+      pjTitle: "Number Guesser",
+      pjDetail:
+      "A little game that mixed the HTML5, CSS, and JavaScript languages. I learned, this game is the system randomly picks a number, then the player and the computer each pick a number, and the party closest to the system's random number wins.",
+      src: NumberGuesser,
+      alt: "Number Guesser Side Project",
+      style:{opacity: 3}
+    },
+    {
+      id: "jammming",
+      pjTitle: "Jammming",
+      pjDetail:
+      "In this side project, I build a React web application called. Jammming. I use React components, passing state, and requests with the Spotify API to build a website that allows users to search the Spotify library, create a custom playlist, then save it to their Spotify account.",
+      src: Jammming,
+      alt: "Jammming Side Project",
+      style:{opacity: 4}
+    },
+    {
+      id: "toDoList",
+      pjTitle: "To Do List",
+      pjDetail:
+      "It's a one-month side project by three people, that uses all the knowledge I've learned, starting with design, exchanging ideas with team members, and sharing suggestions, a web app that can really be used by others.",
+      src: ToDoList,
+      alt: "To Do List Side Project",
+      style:{opacity: 5}
+    },
+  ];
 
   const [isVisible, setVisible] = useState(false);
   const domRef = React.useRef();
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
-        // In your case there's only one element to observe:
         if (entries[0].isIntersecting) {
-          // Not possible to set it back to false like this:
           setVisible(true);
-
-          // No need to keep observing:
-          observer.unobserve(domRef.current);
         }
       },
       {
@@ -44,124 +81,48 @@ export default function Project() {
     <div className="projectList">
       <span>──────&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;My Side Project</span>
       <div className="sideProjectList">
-        <div
-          id="colmarAcademy"
-          ref={domRef}
-          className={`sideProject fade-in-section ${
-            isVisible ? "is-visible" : ""
-          }`}
-        >
-          <div className="sideProject-text">
-            <h3>Colmar Academy</h3>
-            <p>
-              My first side project, a web template need to use HTML5, CSS, and
-              Responsive Web Design as project requirements. Outside of the
-              project requirements, I joined the bootstrap framework to complete
-              this side project.
-            </p>
-            <Link to="#" className="readMore-btn">
-              Read More
-            </Link>
-          </div>
-          <div className="sideProject-img">
-            <img src={ColmarAcademy} alt="Colmar Academy Side Project" />
-          </div>
-        </div>
-        <div
-          id="findYourHat"
-          ref={domRef}
-          className={`sideProject fade-in-section ${
-            isVisible ? "is-visible" : ""
-          }`}
-        >
-          <div className="sideProject-img">
-            <img src={FindYourHat} alt="Find Your Hat Side Project" />
-          </div>
-          <div className="sideProject-text">
-            <h3>Find Your Hat</h3>
-            <p>
-              This is my first side project after learning JavaScript, only
-              using four patterns and JS language to create this mini-game that
-              can run in a terminal. To add challenges, in addition to the
-              project requirements, I added Hardmode to make this mini-game more
-              interesting.
-            </p>
-            <Link to="#" className="readMore-btn">
-              Read More
-            </Link>
-          </div>
-        </div>
-        <div
-          id="numberGuesser"
-          ref={domRef}
-          className={`sideProject fade-in-section ${
-            isVisible ? "is-visible" : ""
-          }`}
-        >
-          <div className="sideProject-text">
-            <h3>Number Guesser</h3>
-            <p>
-              A little game that mixed the HTML5, CSS, and JavaScript languages
-              I learned, this game is the system randomly picks a number, then
-              the player and the computer each pick a number, and the party
-              closest to the system's random number wins.
-            </p>
-            <Link to="#" className="readMore-btn">
-              Read More
-            </Link>
-          </div>
-          <div className="sideProject-img">
-            <img src={NumberGuesser} alt="Number Guesser Side Project" />
-          </div>
-        </div>
-        <div
-          id="jammming"
-          ref={domRef}
-          className={`sideProject fade-in-section ${
-            isVisible ? "is-visible" : ""
-          }`}
-        >
-          <div className="sideProject-img">
-            <img src={Jammming} alt="Jammming Side Project" />
-          </div>
-          <div className="sideProject-text">
-            <h3>Jammming</h3>
-            <p>
-              In this side project, I build a React web application called
-              Jammming. I use React components, passing state, and requests with
-              the Spotify API to build a website that allows users to search the
-              Spotify library, create a custom playlist, then save it to their
-              Spotify account.
-            </p>
-            <Link to="#" className="readMore-btn">
-              Read More
-            </Link>
-          </div>
-        </div>
-        <div
-          id="toDoList"
-          ref={domRef}
-          className={`sideProject fade-in-section ${
-            isVisible ? "is-visible" : ""
-          }`}
-          style={{ padding: "10px" }}
-        >
-          <div className="sideProject-text">
-            <h3>To Do List</h3>
-            <p>
-              It's a one-month side project by three people, that uses all the
-              knowledge I've learned, starting with design, exchanging ideas
-              with team members, and sharing suggestions, a web app that can
-              really be used by others.
-            </p>
-            <Link to="#" className="readMore-btn">
-              Read More
-            </Link>
-          </div>
-          <div className="sideProject-img">
-            <img src={ToDoList} alt="To Do List Side Project" />
-          </div>
-        </div>
+        {pjData.map((pj, col) => {
+          return col % 2 == 0 ? (
+            <div
+              id={pj.id}
+              ref={domRef}
+              className={`sideProject fade-in-section ${
+                isVisible ? "is-visible" : ""
+              }`}
+              style={pj.style}
+            >
+              <div className="sideProject-text">
+                <h3>{pj.pjTitle}</h3>
+                <p>{pj.pjDetail}</p>
+                <Link to="#" className="readMore-btn">
+                  Read More
+                </Link>
+              </div>
+              <div className="sideProject-img">
+                <img src={pj.src} alt={pj.alt} />
+              </div>
+            </div>
+          ) : (
+            <div
+              id={pj.id}
+              ref={domRef}
+              className={`sideProject fade-in-section ${
+                isVisible ? "is-visible" : ""
+              }`}
+            >
+              <div className="sideProject-img">
+                <img src={pj.src} alt={pj.alt} />
+              </div>
+              <div className="sideProject-text">
+                <h3>{pj.pjTitle}</h3>
+                <p>{pj.pjDetail}</p>
+                <Link to="#" className="readMore-btn">
+                  Read More
+                </Link>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
