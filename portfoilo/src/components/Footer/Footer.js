8ad5ddd9c439dbac3b484a15be5878042c18232e
Andison <andison624@gmail.com>
2022-02-18 00:40:12 +0800

    commit

--- portfoilo/src/components/Footer/Footer.js
+++ portfoilo/src/components/Footer/Footer.js
@@ -10,18 +10,37 @@ export default function Footer() {
     <div className="footer">
       <div className="footer-inner">
         <div className="contact">
-          <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=andison624@gmail.com" target="_blank" rel="email" >
+          <a
+            href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=andison624@gmail.com"
+            target="_blank"
+            rel="email"
+          >
             <h3>andison624@gmail.com</h3>
           </a>
           <p>Get in touch with me just click these. Let's connect.</p>
           <div className="footer-img">
-            <a href="https://github.com/Andison624" target="_blank" rel="github" className="footer-img-inner">
+            <a
+              href="https://github.com/Andison624"
+              target="_blank"
+              rel="github"
+              className="footer-img-inner"
+            >
               <img src={github}></img>
             </a>
-            <a href="https://www.linkedin.com/in/andison624/" target="_blank" rel="linkedin" className="footer-img-inner">
+            <a
+              href="https://www.linkedin.com/in/andison624/"
+              target="_blank"
+              rel="linkedin"
+              className="footer-img-inner"
+            >
               <img src={linkedin}></img>
             </a>
-            <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=andison624@gmail.com" target="_blank" rel="email" className="footer-img-inner">
+            <a
+              href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=andison624@gmail.com"
+              target="_blank"
+              rel="email"
+              className="footer-img-inner"
+            >
               <img src={mail}></img>
             </a>
           </div>
